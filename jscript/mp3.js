d032ab22dd50d000b7b6f5dc22416b2c2a7f4baf
KodiClair <kclair165@gmail.com>
2024-05-29 13:57:56 -0700

    Added mp3.js code, changed play_vid function

---
+++ jscript/mp3.js
@@ -0,0 +1,21 @@
+const video = videoPlayer.querySelector('.video');
+
+function menu() {
+    location.href="../index.html";
+};
+
+function play_vid() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+};
+
+function skip() {
+    video.s
+};
+
+function previous() {
+
+};
